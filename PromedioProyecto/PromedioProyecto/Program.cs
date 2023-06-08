fb994ff482b825df96c3d3d7ff11d6fe344c9158
ale-silva3 <alesilvainsed143@gmail.com>
2023-06-08 20:58:06 -0300

    actividad #1 lista

--- PromedioProyecto/PromedioProyecto/Program.cs
+++ PromedioProyecto/PromedioProyecto/Program.cs
@@ -10,6 +10,50 @@ namespace PromedioProyecto
     {
         static void Main(string[] args)
         {
+            double valor;
+            int opcion;
+
+            Promedio myPromedio = new Promedio();
+
+            do 
+            {
+                Console.WriteLine("1. Ingresar Numero");
+                Console.WriteLine("2. Mostrar Promedio");
+                Console.WriteLine("3. Salir ");
+                opcion = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcion) 
+                {
+                    case 1:
+                        Console.WriteLine("Ingrese un numero o -1 para salir");
+                        valor = Convert.ToDouble(Console.ReadLine());
+
+                        while(valor >= 0)
+                        {
+                            myPromedio.AgregarValor(valor);
+                            Console.WriteLine("Ingrese un numero o -1 para salir");
+                            valor = Convert.ToDouble(Console.ReadLine());
+                        }
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Promedio: {0}",myPromedio.CalcularPromedio());
+                        Console.ReadKey();
+                        break;
+
+                    case 3:
+                        break;
+
+                    default:
+                        Console.WriteLine("Opcion Incorrecta");
+                        Console.ReadKey();
+                        break;
+                }
+
+                Console.Clear();
+            } 
+            while (opcion != 3);
+
         }
     }
 }
--- PromedioProyecto/PromedioProyecto/Promedio.cs
+++ PromedioProyecto/PromedioProyecto/Promedio.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace PromedioProyecto
 {
-    class Promedio
+    internal class Promedio
     {
+        public double acum = 0;
+        public int contador = 0;
+
+        public Promedio(){}
+        public void AgregarValor(double valor)
+        {
+            acum += valor;
+            contador++;
+        }
+
+        public double CalcularPromedio()
+        {
+            double promedio = 0;
+
+            if (contador > 0)
+            {
+                promedio = acum / contador;
+            }
+
+            return promedio;
+        }
+
     }
 }
